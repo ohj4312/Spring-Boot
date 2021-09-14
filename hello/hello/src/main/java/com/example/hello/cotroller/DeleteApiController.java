6267ffaa61546f1c8185af18ca992c8f5b80d785
ohj4312@naver.com <ohj4312@naver.com>
2021-09-15 04:07:21 +0900

    delete source code

---
+++ hello/hello/src/main/java/com/example/hello/cotroller/DeleteApiController.java
@@ -0,0 +1,17 @@
+package com.example.hello.cotroller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class DeleteApiController {
+
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account){
+        System.out.println(userId);
+        System.out.println(account);
+
+        //리소스가 없다는 값을 던질 필요가 없다.
+        // delete -> 리소스 삭제이므로 있어서 삭제를 해서 없는 상태와 원래 없는 상태와 모두 삭제된 상태인 것으로 보아 200 code를 준다.
+    }
+}
